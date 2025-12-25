b75464e9b40643d71d3604a7899d0f8364fc413a
xjanikov <536473@mail.muni.cz>
2025-12-25 13:55:42 +0100

    Add new layer window layout changes

--- pro-pain-t-app/src/components/new_layer_window.rs
+++ pro-pain-t-app/src/components/new_layer_window.rs
@@ -8,16 +8,15 @@ pub fn NewLayerWindow(dialog_ref: NodeRef<Dialog>, is_open: RwSignal<bool>) -> i
         <dialog
             node_ref = dialog_ref
             style = move || {format!("align-items:center;
-                justify-content:center;
+                justify-content:space-between;
                 background:#3f3f3f;
-                width:400px;
-                flex-direction:column;
+                flex-direction:row;
                 display:{};", if is_open.get() {"block"} else {"none"})}>
             <h1 style="color:white; text-align:center;">"New layer"</h1>
             <table>
                 <tr>
                     <td style:color="white">"Title:"</td>
-                    <td>
+                    <td >
                         <input type="text" color="black" text="Title" id="new-layer-title"/>
                     </td>
                 </tr>
@@ -26,40 +25,45 @@ pub fn NewLayerWindow(dialog_ref: NodeRef<Dialog>, is_open: RwSignal<bool>) -> i
                     <td><ColorPicker /></td>
                 </tr>
             </table>
-            <button
-                on:click = move |_| {
-                    is_open.set(false);
-                    dialog_ref.get().unwrap().close();
-                }
-                id="cancel-add-layer-window"
-                style="
-                    margin-top:0.25rem;
-                    padding:0.25rem 0.5rem;
-                    border-radius:2px;
-                    border:none;
-                    background:#3a3a3a;
-                    color:#f5f5f5;
-                    font-size:0.8rem;
-                    text-align:left;
-                "
-            >"Cancel"</button>
-            <button
-                on:click = move |_| {
-                    is_open.set(false);
-                    dialog_ref.get().unwrap().close();
-                }
-                id="confirm-add-layer-window"
-                style="
-                    margin-top:0.25rem;
-                    padding:0.25rem 0.5rem;
-                    border-radius:2px;
-                    border:none;
-                    background:#3a3a3a;
-                    color:#f5f5f5;
-                    font-size:0.8rem;
-                    text-align:left;
-                "
-            >"Ok"</button>
+            <div
+            style="display:flex; justify-content:space-between;">
+                <button
+                    on:click = move |_| {
+                        is_open.set(false);
+                        dialog_ref.get().unwrap().close();
+                    }
+                    id="cancel-add-layer-window"
+                    style="
+                        margin-top:0.25rem;
+                        padding:0.25rem 0.5rem;
+                        border-radius:2px;
+                        border:none;
+                        background:#3a3a3a;
+                        color:#f5f5f5;
+                        font-size:0.8rem;
+                        text-align:center;
+                        width:70px;
+                    "
+                >"Cancel"</button>
+                <button
+                    on:click = move |_| {
+                        is_open.set(false);
+                        dialog_ref.get().unwrap().close();
+                    }
+                    id="confirm-add-layer-window"
+                    style="
+                        margin-top:0.25rem;
+                        padding:0.25rem 0.5rem;
+                        border-radius:2px;
+                        border:none;
+                        background:#3a3a3a;
+                        color:#f5f5f5;
+                        font-size:0.8rem;
+                        text-align:center;
+                        width:70px;
+                    "
+                >"Ok"</button>
+            </div>
         </dialog>
     }
 }
