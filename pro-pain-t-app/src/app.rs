ac360ca30469eeb71a3af9106620445f3b3ddebc
redbeam_ <skvarlamatus@gmail.com>
2026-01-13 17:01:50 +0100

    feat: add autosave timer

--- pro-pain-t-app/src/app.rs
+++ pro-pain-t-app/src/app.rs
@@ -8,7 +8,7 @@ use crate::view_state::ProjectViewState;
 use leptos::html::Dialog;
 use leptos::prelude::*;
 use pro_pain_t_app::structs::project::Project;
-use std::fs;
+use std::{env, fs};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -37,6 +37,11 @@ pub fn App() -> impl IntoView {
         let project_file_save_path = String::new(); // TODO receive from event
         let project_serialized: Vec<u8> = Vec::new(); // TODO serialize project
         fs::write(project_file_save_path, project_serialized).expect("Failed to write to file");
+    };
+
+    let _autosave_handler = || {
+        let _autosave_path = env::temp_dir().set_file_name(format!("{}_autosave.ppp", project.get().name));
+        // TODO call save project handler with this path
     };
 
     view! {
