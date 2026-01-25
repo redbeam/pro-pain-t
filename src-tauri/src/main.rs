7901b1bc17ff6148891163dc99c6f6eadfecb5fd
redbeam_ <skvarlamatus@gmail.com>
2026-01-25 21:49:16 +0100

    feat: rework of event system + export functionality
    Complex rework of event system to enable backend and frontend communication.
    Export functionality was also added as a part of this.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,20 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod menus;
+mod events;
 
-use std::time::Duration;
-use tauri::Emitter;
 use crate::menus::setup_menus;
-
-const AUTOSAVE_INTERVAL: u32 = 180; // seconds
+use crate::events::commands::{export_project_command, save_project_command};
+use std::env;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             setup_menus(app)?;
-
-            let app_handle = app.handle().clone();
-            std::thread::spawn(move || loop {
-                std::thread::sleep(Duration::from_secs(AUTOSAVE_INTERVAL as u64));
-                app_handle.emit("autosave-project", ())
-                    .expect("Failed to emit autosave-project");
-                println!("emitted autosave-project");
-            });
-
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![save_project_command, export_project_command])
         .run(tauri::generate_context!())
-        .expect("error while running Pro PainT Tauri application");
+        .expect("Error while running Pro PainT Tauri application");
 }
